3d6c84e96e4c6082cf440f7247a84cbb9537d318
AcquaDiGiorgio <jorgelisa.2000@gmail.com>
2021-09-26 19:11:35 +0200

    Test Funcionales
    Tests Dial - Listen funcionales con esperas para que terminen correctamente

--- TestDial.go
+++ TestDial.go
@@ -1,32 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"time"
 )
 
+func checkError(err error, str string) {
+	if err != nil {
+		fmt.Print("ERROR EN " + str)
+	}
+}
+
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8080")
 	conn.SetDeadline(time.Now().Add(time.Hour))
 
+	// Cierra la conexión cuando termina la ejecución del main
 	defer conn.Close()
-
-	if err != nil {
-		// handle error
-	}
+	checkError(err, "Dial")
 	/*
 		Envío de mensajes
 	*/
-	b := []byte("Hola")
+	b := []byte("Hola\x00") // terminador x000 == \0 en Java o C
 	trueW, _ := conn.Write(b)
 	fmt.Print("Bytes Written - ", trueW, "\n")
 	/*
 		Recepción de mensajes
 	*/
-	var read []byte
-	trueR, _ := conn.Read(read)
-	fmt.Print("Bytes Read - ", trueR)
+	read, _ := bufio.NewReader(conn).ReadBytes('\x00')
 
-	fmt.Print(string(read))
+	fmt.Print("Mesaje Recibido: " + string(read) + "\n")
 }
--- TestListen.go
+++ TestListen.go
@@ -1,44 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"strconv"
+	"sync"
 	"time"
 )
 
-func main() {
-	ln, err := net.Listen("tcp", ":8080")
-	defer ln.Close()
-
+func checkError(err error, str string) {
 	if err != nil {
-		fmt.Print("ERROR en LISTEN")
+		fmt.Print("ERROR EN " + str)
 	}
+}
 
-	for {
+func main() {
+	var wg sync.WaitGroup // Creamos un semaforo que espera a todos los clientes
+	ln, err := net.Listen("tcp", ":8080")
+	// Cierra la conexión cuando termina la ejecución del main (la escucha en el puerto 8080)
+	defer ln.Close()
+	defer fmt.Print("Cerramos Puerto\n")
+
+	checkError(err, "Listen")
+
+	for i := 0; i < 2; i++ {
+		fmt.Print("Iter numero: " + strconv.Itoa(i) + "\n")
 		conn, err := ln.Accept()
+		checkError(err, "Accept")
+
+		// Tiempo Límite que puede estar una conexión mantenida (1 hora en este caso)
 		conn.SetDeadline(time.Now().Add(time.Hour))
 
-		if err != nil {
-			fmt.Print("ERROR en ACCEPT\n")
-		}
-
 		go func(c net.Conn) {
+			wg.Add(1)
+			// Cierra la conexión cuando termina la ejecución de la función integrada (la conexión con un cliente)
 			defer c.Close()
+			// Decimos al semaforo que hemos terminado cuando termiene el thread
+			defer wg.Done()
+			defer fmt.Print("Cerramos Conexión\n")
 			/*
 				Recepción de mensajes
 			*/
-			var read []byte
-			trueR, _ := c.Read(read)
-			fmt.Print("Bytes Read - ", trueR, "\n")
+			read, _ := bufio.NewReader(c).ReadBytes('\x00')
+			fmt.Print("Mesaje Recibido: " + string(read) + "\n")
 			/*
 				Envío de mensajes
 			*/
-			b := []byte("Adios")
+			b := []byte("Adios\x00") // terminador x000 == \0 en Java o C || También valdría \000
 			trueW, _ := c.Write(b)
 			fmt.Print("Bytes Written - ", trueW, "\n")
-
-			fmt.Print(string(read))
-		}(conn)
-
+			c.Close()
+		}(conn) // Parámetro que pasamos a la función embebida
 	}
+	// Esperamos a que terminen todos
+	wg.Wait()
 }
