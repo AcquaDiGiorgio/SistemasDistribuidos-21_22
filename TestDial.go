bf9ca8747fc9b526b9e64faf1ab498e7b4f096e1
AcquaDiGiorgio <jorgelisa.2000@gmail.com>
2021-09-27 14:58:34 +0200

    Corrección Tests

--- TestDial.go
+++ TestDial.go
@@ -1,16 +1,60 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"time"
 )
 
-func checkError(err error, str string) {
+func checkError(err error) {
 	if err != nil {
-		fmt.Print("ERROR EN " + str)
+		fmt.Print("ERROR - " + err.Error())
 	}
+}
+
+// No es necesario implementarla
+/*
+	Pre: Conexión con el Servidor
+	Post: Cadena de enteros sin signo leidos
+
+	Función especial que lee una muletilla y luego el resto del
+	mensaje.
+	La muletilla marca el tipo de máquina (Big endian o Little endian)
+	que es el servidor y posteriormente trata el mensaje dependiendo
+	de esta muletilla.
+*/
+func read(conn net.Conn) []uint16 {
+	var buf [512]byte
+	// Leemos primero el BOM
+	bytesRead, err := conn.Read(buf[0:2])
+	checkError(err)
+
+	// Leemos el resto del Mensaje
+	for true {
+		m, err := conn.Read(buf[bytesRead:])
+		if m == 0 || err != nil {
+			break
+		}
+		bytesRead += m
+	}
+
+	// Creamos la respuesta de tamaño bytesRead/2 (de bytes a uint16: tamaño 8 a tamaño 16)
+	var respuesta []uint16
+	respuesta = make([]uint16, bytesRead/2)
+
+	// Realizamos una cosa u otra dependiendo del timpo de máquina que es
+	if buf[0] == 0xff && buf[1] == 0xfe { // Big Endian
+		for i := 2; i < bytesRead; i += 2 {
+			respuesta[i/2] = uint16(buf[i])<<8 + uint16(buf[i+1])
+		}
+	} else if buf[1] == 0xff && buf[0] == 0xfe { // Little Endian
+		for i := 2; i < bytesRead; i += 2 {
+			respuesta[i/2] = uint16(buf[i+1])<<8 + uint16(buf[i])
+		}
+	} else { // Orden no Identificado
+		return nil
+	}
+	return respuesta
 }
 
 func main() {
@@ -19,17 +63,18 @@ func main() {
 
 	// Cierra la conexión cuando termina la ejecución del main
 	defer conn.Close()
-	checkError(err, "Dial")
+	checkError(err)
 	/*
 		Envío de mensajes
 	*/
-	b := []byte("Hola\x00") // terminador x000 == \0 en Java o C
+	b := []byte("Hola")
 	trueW, _ := conn.Write(b)
-	fmt.Print("Bytes Written - ", trueW, "\n")
+	fmt.Print("Bytes Escritos - ", trueW, "\n")
 	/*
 		Recepción de mensajes
 	*/
-	read, _ := bufio.NewReader(conn).ReadBytes('\x00')
+	var lectura [512]byte
+	n, _ := conn.Read(lectura[:])
 
-	fmt.Print("Mesaje Recibido: " + string(read) + "\n")
+	fmt.Print("Mesaje Recibido: " + string(lectura[:n]) + "\n")
 }
--- TestListen.go
+++ TestListen.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -17,23 +15,24 @@ func checkError(err error, str string) {
 
 func main() {
 	var wg sync.WaitGroup // Creamos un semaforo que espera a todos los clientes
+	// Empezamos a ecuchar en el puerto y checkeamos error
 	ln, err := net.Listen("tcp", ":8080")
+	checkError(err, "Listen")
 	// Cierra la conexión cuando termina la ejecución del main (la escucha en el puerto 8080)
 	defer ln.Close()
 	defer fmt.Print("Cerramos Puerto\n")
 
-	checkError(err, "Listen")
-
+	// Aceptamos solo 2 Clientes
 	for i := 0; i < 2; i++ {
-		fmt.Print("Iter numero: " + strconv.Itoa(i) + "\n")
+		// Abrimos conexión con un Cliente y comprobamos que todo esté correcto
 		conn, err := ln.Accept()
 		checkError(err, "Accept")
-
+		// Añadimos un proceso a la espera
+		wg.Add(1)
 		// Tiempo Límite que puede estar una conexión mantenida (1 hora en este caso)
 		conn.SetDeadline(time.Now().Add(time.Hour))
 
 		go func(c net.Conn) {
-			wg.Add(1)
 			// Cierra la conexión cuando termina la ejecución de la función integrada (la conexión con un cliente)
 			defer c.Close()
 			// Decimos al semaforo que hemos terminado cuando termiene el thread
@@ -42,17 +41,16 @@ func main() {
 			/*
 				Recepción de mensajes
 			*/
-			read, _ := bufio.NewReader(c).ReadBytes('\x00')
-			fmt.Print("Mesaje Recibido: " + string(read) + "\n")
+			var lectura [512]byte                                       // buffer de max 512 bytes
+			n, _ := conn.Read(lectura[:])                               // Leemos todo el buffer
+			fmt.Print("Mesaje Recibido: " + string(lectura[:n]) + "\n") // Mostramos hasta el tam leido
 			/*
 				Envío de mensajes
 			*/
-			b := []byte("Adios\x00") // terminador x000 == \0 en Java o C || También valdría \000
-			trueW, _ := c.Write(b)
-			fmt.Print("Bytes Written - ", trueW, "\n")
-			c.Close()
+			b := []byte("Adios")   // terminador x000 == \0 en Java o C || También valdría \000
+			trueW, _ := c.Write(b) // Escribimos a través de la conexión
+			fmt.Print("Bytes Escritos - ", trueW, "\n")
 		}(conn) // Parámetro que pasamos a la función embebida
 	}
-	// Esperamos a que terminen todos
-	wg.Wait()
+	wg.Wait() // Esperamos a que terminen todos
 }
